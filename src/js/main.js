0de5682b7d90c24680e86ec5fe7d24774b9dee80
wawanzs <syzwnzhr@gmail.com>
2015-04-28 17:36:38 +0800

    testing the animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function() {
+	$('.box').css({
+		'height' : $(window).height(),
+		'width'  : $(window).width()
+	});
+	$('.launcher a').on('click', function(e) {
+		e.preventDefault();
+		$('#fly-me').addClass('to-the-moon');
+	});
+});
