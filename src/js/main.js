098d712b7017078c20e1e1f9f12e08213ee29425
wawanzs <syzwnzhr@gmail.com>
2015-04-30 20:37:39 +0800

    added animation on the launcher button

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
+	
 	$('.box').css({
 		'height' : $(window).height(),
 		'width'  : $(window).width()
 	});
+
 	$('.launcher a').on('click', function(e) {
 		e.preventDefault();
+		
 		$('#fly-me').addClass('to-the-moon');
+		
+		$(this).parent().delay(1000).queue(function() {
+			$(this).addClass('hide-me').dequeue();
+		});
+
+		setTimeout(function() {
+			$('.launcher').removeClass('hide-me');
+			$('#fly-me').removeClass('to-the-moon');
+		},8000);
+		
 	});
 });
